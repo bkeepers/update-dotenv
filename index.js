8a15018aed4c1a2958505e939f95b38a9a8bcd61
Brandon Keepers <bkeepers@github.com>
2018-08-10 13:19:53 -0400

    Add standard linter, fix errors

--- index.js
+++ index.js
@@ -15,9 +15,13 @@ module.exports = async function updateDotenv (env) {
   const filename = path.join(process.cwd(), '.env')
 
   // Merge with existing values
-  if (await promisify(fs.exists)(filename)) {
+  try {
     const existing = dotenv.parse(await promisify(fs.readFile)(filename, 'utf-8'))
     env = Object.assign(existing, env)
+  } catch (err) {
+    if (err.code !== 'ENOENT') {
+      throw err
+    }
   }
 
   const contents = Object.keys(env).map(key => format(key, env[key])).join('\n')
